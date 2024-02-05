6f65d20c7c83db552e82362562ee37f8e8d97d5c
Amarildo <amarildos@outloo.it>
2024-02-05 17:15:27 +0100

    imported sass

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
 export default {
-
+  name: 'vite-Yu-Gi-Oh',
 };
 
 </script>
 
 <template></template>
 
-<style></style>
+<style lang="scss">
+@use'../src/assets/scss/style.scss';
+</style>
